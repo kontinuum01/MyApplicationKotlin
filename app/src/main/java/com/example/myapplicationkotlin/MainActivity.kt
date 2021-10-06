5068ef47ada1d9adada4be527112ffd3c8a0000a
Владислав <kontinuum01@gmail.com>
2021-10-07 01:24:29 +0300

    Kotlin

--- app/src/main/java/com/example/myapplicationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationkotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplicationkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView: MaterialTextView = findViewById(R.id.text)
 
+        val button: MaterialButton = findViewById(R.id.button)
+        button.setOnClickListener {
+            textView.text = "Hello World!"
+
+
+        }
     }
 }
 
